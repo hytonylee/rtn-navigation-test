3a129ffc373c420083027e68429673e5b8de995c
Tony Lee <hy.tony.lee@gmail.com>
2020-03-01 21:27:40 -0800

    Add navigation.push on DetailScreen

--- components/screens/DetailScreen.js
+++ components/screens/DetailScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
-const DetailScreen = () => {
+const DetailScreen = ({navigation}) => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go Back Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+      <Button
+        title="Go to Detail Again"
+        onPress={() => navigation.push('Detail')}
+      />
     </View>
   );
 };
